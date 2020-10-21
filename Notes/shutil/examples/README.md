8761089633bc77f790278b2b21edbc28693bd692
Song <song_csu@outlook.com>
2020-10-22 00:20:05 +0800

    学习前背景色输出

---
+++ Notes/shutil/examples/README.md
@@ -0,0 +1,7 @@
+## sort_failimg2.py
+
+### TODO
+
+- [ ] 添加启动 LOGO  
+- [ ] 设置命令行参数  
+- 
---
+++ Notes/terminal/art_text.py
@@ -0,0 +1,15 @@
+# 默认
+print('\033[0;30;43mHello \033[0;37;41mFoxconn\033[0m')
+
+# 高亮
+print('\033[1;30;43mHello \033[1;37;41mFoxconn\033[0m')
+
+# 
+print('\033[2;30;43mHello \033[2;37;41mFoxconn\033[0m')
+
+# 下划线
+print('\033[4;30;43mHello \033[4;37;41mFoxconn\033[0m')
+
+# 反显
+print('\033[7;30;43mHello \033[7;37;41mFoxconn\033[0m')
+
